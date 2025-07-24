7b75d4fbca1a45066e00754a491e7d7247397116
RinLit <lintusup@foxmail.com>
2025-07-24 18:06:00 +0800

    123

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="adventure-x-shit",
+    name="adventure-x-shitventure",
     version="0.114.514",
     packages=find_packages(),
     install_requires=[
